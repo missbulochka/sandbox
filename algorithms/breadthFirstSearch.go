3446b0fa532378978b4ce7783e6874092db8355a
elina <elina.paxar@gmail.com>
2023-09-03 16:00:01 +0300

    add breadth first search. I should add path length

---
+++ algorithms/breadthFirstSearch.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+)
+
+func makeGraph() map[uint8][]uint8 {
+	graph := make(map[uint8][]uint8)
+
+	graph[1] = []uint8{3, 4, 6}
+	graph[2] = []uint8{1}
+	graph[3] = []uint8{2, 5}
+	graph[4] = []uint8{6}
+	graph[5] = []uint8{}
+	graph[6] = []uint8{4}
+	graph[7] = []uint8{4}
+
+	return graph
+}
+
+func findInSearched(searched []uint8, node uint8) bool {
+	for _, el := range searched {
+		if el == node {
+			return true
+		}
+	}
+	return false
+}
+
+func BFS(graph map[uint8][]uint8, start, end uint8) bool {
+	var queue []uint8
+	var searched []uint8
+	queue = append(queue, graph[start]...)
+
+	for len(queue) > 0 {
+		if !findInSearched(searched, queue[0]) {
+			if queue[0] == end {
+				return true
+			} else {
+				searched = append(searched, queue[0])
+				queue = append(queue, graph[queue[0]]...)
+			}
+		}
+		queue = queue[1:]
+	}
+	return false
+}
+
+func main() {
+	graph := makeGraph()
+
+	fmt.Println(graph)
+	if BFS(graph, 1, 5) {
+		fmt.Println("Path found!")
+	} else {
+		fmt.Println("Path not found")
+	}
+}
